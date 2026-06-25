fd815e85ca4c4014b47f04ffe870ab85706ef869
ashritha-prog <ashritha21105@gmail.com>
2026-06-25 22:25:10 +1100

    Python basics

---
+++ text.py
@@ -0,0 +1,24 @@
+# # # #print("HELLO WORLD",end=" ")
+# # # #print("HELLO WORLD",end=" ")
+# # # #print("HELLO WORLD","WORLD",sep="#")
+# # # x="10.5"
+# # # #Y="s"
+# # # #print("abc">"abd")
+# # # #x=input() #sring
+# # # #s=int(input()) #integer
+# # # #x=int(x)
+# # # #x=int(float(x))
+# # # #print(x)
+# # # print("\test\ntesting")
+# # # #print(r"\test\ntesting")
+# # # #print("\\test\\ntesting")
+# # print("""hello
+# #       my name is 
+# #       john
+# #       """)
+# x="hello"
+# print(x[0:3:2])
+# print(x[::-1])
+for i in range(0,10,2):
+    #if i%2==0:
+        print(i)
